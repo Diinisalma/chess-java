19048bff926e6bb781d6073b83c1ab3294ad01fc
Diini Salma <op44@fifgroup.co.id>
2025-02-26 20:01:02 +0700

    add(rook) : Add rook movements

--- src/main/java/com/github/diinisalma/chess/piece/Rook.java
+++ src/main/java/com/github/diinisalma/chess/piece/Rook.java
@@ -1,5 +1,6 @@
 package com.github.diinisalma.chess.piece;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import com.github.diinisalma.chess.Board;
@@ -14,8 +15,48 @@ public class Rook extends Piece {
 
     @Override
     List<Point> getAvailableMoves(Point currPosition) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getAvailableMoves'");
+        List<Point> movements = new ArrayList<>();
+        // Get the current position of the Rook
+        int x = currPosition.getX();
+        int y = currPosition.getY();
+
+        // Check movements in the x-direction (left and right)
+        for (int i = x - 1; i >= 0; i--) {
+            Point newPosition = new Point(i, y);
+            if (isValidMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+        for (int i = x + 1; i < 8; i++) {
+            Point newPosition = new Point(i, y);
+            if (isValidMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+
+        // Check movements in the y-direction (up and down)
+        for (int i = y - 1; i >= 0; i--) {
+            Point newPosition = new Point(x, i);
+            if (isValidMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+        for (int i = y + 1; i < 8; i++) {
+            Point newPosition = new Point(x, i);
+            if (isValidMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+
+        return movements;
     }
 
     @Override
@@ -24,9 +65,60 @@ public class Rook extends Piece {
     }
 
     @Override
-    public boolean move(Point position, PieceColor color) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'move'");
+    public Piece killingMove(Point nextPosition) {
+        List<Point> movements = new ArrayList<>();
+        // Get the current position of the Rook
+        int x = currPosition.getX();
+        int y = currPosition.getY();
+
+        // Check movements in the x-direction (left and right)
+        for (int i = x - 1; i >= 0; i--) {
+            Point newPosition = new Point(i, y);
+            if (isValidKillingMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+        for (int i = x + 1; i < 8; i++) {
+            Point newPosition = new Point(i, y);
+            if (isValidKillingMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+
+        // Check movements in the y-direction (up and down)
+        for (int i = y - 1; i >= 0; i--) {
+            Point newPosition = new Point(x, i);
+            if (isValidKillingMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+        for (int i = y + 1; i < 8; i++) {
+            Point newPosition = new Point(x, i);
+            if (isValidKillingMove(newPosition)) {
+                movements.add(newPosition);
+            } else {
+                break;
+            }
+        }
+
+        if (movements.contains(nextPosition)) {
+            this.setPrevPosition(currPosition);
+            this.setCurrPosition(nextPosition);
+        } else {
+            System.out.println("Invalid move");
+        }
+
+        return this;
+    }
+
+    private boolean isValidKillingMove(Point newPosition) {
+        return !Board.board[newPosition.getX()][newPosition.getY()].contains(color.toString());
     }
 
 }
